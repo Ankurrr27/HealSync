d61429f044c712d156929af3493deeadb9fb50ad
Ankur <ankurp22singh@gmail.com>
2025-09-26 11:54:10 +0530

    updated path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Home from "./pages/home";
 import About from "./pages/about";
 import ConsultAI from "./pages/ConsultAI";
 
